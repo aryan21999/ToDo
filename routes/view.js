e5e6922bc95148c9384b29f3624f10024d9c82f5
Aryan <test@test.com>
2021-02-11 12:54:52 +0530

    initial commit

--- routes/view.js
+++ routes/view.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var auth = function (req, res, next) {
+  if (req.session.email && req.session.isLogged)
+      return next();
+  else
+      return res.redirect('/');
+};
+
+router.get('/home', auth, function (req, res, next) {
+  if (req.session.isLogged) {
+    res.render('home', { title: req.session.email })
+  }
+});
+
+/* GET login page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('welcome', { title: 'MyTodosApp' });
+});
+
+router.get('/signin', function(req, res, next ){
+  res.render('signin')
+});
+
+router.get('/signup', function(req, res, next ){
+  res.render('signup')
 });
 
 module.exports = router;
