e2f4065206a937b4cbddf97563ccf3d83f6a65ab
Aryan <test@test.com>
2021-02-11 12:32:14 +0530

    second commit

--- routes/api.js
+++ routes/api.js
@@ -10,8 +10,20 @@ var auth = function (req, res, next) {
 };
 
 var authenticate = require('..api/authenticate')
+var addUser = require('../api/addUser')
+var logout = require('../api/logout')
+var addTodo = require('../api/addTodo')
+var listTodo = require('../api/listTodo')
+var deleteTodo = require('../api/deleteTodo')
+var checkTodo =require('../api/checkTodo')
 
-
+router.post('/authenticate', authenticate.authenticate)
+router.post('/add', addUser.addUser)
+router.post('/logout', logout.logout)
+router.post('/add', addTodo.add)
+router.post('/list', listTodo.list)
+router.post('/delete', deleteTodo.delete)
+router.post('/check', checkTodo.check)
 
 
 module.exports = router;
